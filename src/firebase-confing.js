49a9bf0ddfeda1b2144a5598014b64ea8028ec0e
JAlejandroDiaz <jhon.alejandro.diaz@correounivalle.edu.co>
2023-05-16 20:43:59 -0500

    firebase

---
+++ src/firebase-confing.js
@@ -0,0 +1,10 @@
+export default  {
+    apiKey: "AIzaSyBvz_AxYuTaIMzW7E9USWk922LSKKIDNng",
+    authDomain: "imagenes-heroes.firebaseapp.com",
+    databaseURL: "https://imagenes-heroes-default-rtdb.firebaseio.com",
+    projectId: "imagenes-heroes",
+    storageBucket: "imagenes-heroes.appspot.com",
+    messagingSenderId: "465334300605",
+    appId: "1:465334300605:web:0d52fb8011df74b343ecd4",
+    measurementId: "G-2XHQQ64556"
+  };
